85d98a365abdbcc15cc4e94a9dcf94128d8e8f1e
joaquintides <joaquin@tid.es>
2022-02-21 20:09:11 +0100

    erased spurious comment from copied code

--- fca_unordered.hpp
+++ fca_unordered.hpp
@@ -5,8 +5,6 @@
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
- *
- * See http://www.boost.org/libs/multi_index for library home page.
  */
  
 #ifndef FCA_UNORDERED_HPP
