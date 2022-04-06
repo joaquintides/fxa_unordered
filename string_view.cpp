2153a5008f1a22bbfe20627c25f03178a26edacf
joaquintides <joaquin@tid.es>
2022-04-06 19:52:22 +0200

    hope was futile

--- string_view.cpp
+++ string_view.cpp
@@ -279,7 +279,7 @@ int main()
     test<foa_fmod_hcached_unordered_coalesced_map_fnv1a>( "foa_fmod_hcached_unordered_coalesced_map, FNV-1a" );
     test<foa_pow2_fib_unordered_nway_map_fnv1a>( "foa_pow2_fib_unordered_nway_map, FNV-1a" );
     test<foa_pow2_fib_unordered_nwayplus_map_fnv1a>( "foa_pow2_fib_unordered_nwayplus_map, FNV-1a" );
-    test<foa_pow2_fib_unordered_soa_wayplus_map_fnv1a>( "foa_pow2_fib_unordered_soa_nwayplus_map, FNV-1a" );
+    test<foa_pow2_fib_unordered_soa_nwayplus_map_fnv1a>( "foa_pow2_fib_unordered_soa_nwayplus_map, FNV-1a" );
     test<foa_pow2_fib_unordered_coallesced_nwayplus_map_fnv1a>( "foa_pow2_fib_unordered_coallesced_nwayplus_map, FNV-1a" );
     test<foa_pow2_fib_unordered_soa_coallesced_nwayplus_map_fnv1a>( "foa_pow2_fib_unordered_soa_coallesced_nwayplus_map, FNV-1a" );
 
