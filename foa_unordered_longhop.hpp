ad79689b15d46ba59833a51bf5f09c8501f85492
joaquintides <joaquin@tid.es>
2022-04-21 19:27:59 +0200

    made foa_unordered_longhop_set layout SoA

--- foa_unordered_longhop.hpp
+++ foa_unordered_longhop.hpp
@@ -32,12 +32,28 @@ namespace longhop{
 template<typename T>
 struct element
 {
-  static constexpr std::size_t N=16;
-
   T*       data(){return reinterpret_cast<T*>(&storage);}
   const T* data()const{return reinterpret_cast<const T*>(&storage);}
   T&       value(){return *data();}
   const T& value()const{return *data();}
+
+private:
+  std::aligned_storage_t<sizeof(T),alignof(T)> storage;
+};
+
+inline constexpr std::size_t floorlog2(std::size_t n)
+{
+#ifdef _MSC_VER
+  // https://github.com/boostorg/core/issues/109
+  return n==1?0:1+floorlog2(n>>1);
+#else
+  return boost::core::bit_width(n)-1;
+#endif
+}
+
+struct control
+{
+  static constexpr std::size_t N=16;
 
   std::size_t hash()const{return get(width_hash,shift_hash);}
   std::size_t first()const{return get(width_first,shift_first);}
@@ -71,18 +87,7 @@ struct element
   }
 
 private:
-  std::aligned_storage_t<sizeof(T),alignof(T)> storage;
-  uint16_t                                     metadata=0;
-
-  static constexpr std::size_t floorlog2(std::size_t n)
-  {
-#ifdef _MSC_VER
-    // https://github.com/boostorg/core/issues/109
-    return n==1?0:1+floorlog2(n>>1);
-#else
-    return boost::core::bit_width(n)-1;
-#endif
-  }
+  uint16_t metadata=0;
 
   static constexpr std::size_t width=sizeof(metadata)*CHAR_BIT,
                                width_next=floorlog2(N),
@@ -116,7 +121,10 @@ class foa_unordered_longhop_set
   using element_array=std::vector<
     element<T>,
     typename alloc_traits::template rebind_alloc<element<T>>>;
-  static constexpr auto N=element<T>::N;
+  using control_array=std::vector<
+    control,
+    typename alloc_traits::template rebind_alloc<control>>;
+  static constexpr auto N=control::N;
 
 public:
   using key_type=T;
@@ -132,19 +140,20 @@ public:
     friend class foa_unordered_longhop_set;
     friend class boost::iterator_core_access;
 
-    const_iterator(element<T> *p):p{p}{}
+    const_iterator(element<T> *pe,control* pc):pe{pe},pc{pc}{}
 
-    const value_type& dereference()const noexcept{return p->value();}
-    bool equal(const const_iterator& x)const noexcept{return p==x.p;}
-    void increment()noexcept{do{++p;}while(p->empty());}
+    const value_type& dereference()const noexcept{return pe->value();}
+    bool equal(const const_iterator& x)const noexcept{return pe==x.pe;}
+    void increment()noexcept{do{++pe;++pc;}while(pc->empty());}
 
-    element<T> *p=nullptr;
+    element<T> *pe=nullptr;
+    control    *pc=nullptr;
   };
   using iterator=const_iterator;
 
   foa_unordered_longhop_set()
   {
-    elements.back().set_hash(0);
+    controls.back().set_hash(0);
   }
 
   foa_unordered_longhop_set(foa_unordered_longhop_set&&)=default;
@@ -160,7 +169,7 @@ public:
   const_iterator begin()const noexcept
   {
     auto it=at(0);
-    if(it.p->empty())++it;
+    if(it.pc->empty())++it;
     return it;
   }
   
@@ -168,7 +177,9 @@ public:
 
   const_iterator at(std::size_t n)const noexcept
   {
-    return const_cast<element<T>*>(elements.data()+n);
+    return {
+      const_cast<element<T>*>(elements.data()+n),
+      const_cast<control*>(controls.data()+n)};
   }
 
   size_type size()const noexcept{return size_;};
@@ -178,28 +189,28 @@ public:
 
   iterator erase(const_iterator it)
   {    
-    std::size_t pos=it.p-elements.data();
+    std::size_t pos=it.pe-elements.data();
     auto pos1=move_to_end_of_bucket_and_erase(capacity_,pos);
     if(pos1!=capacity_){
-      elements[pos1].set_next(0);
-      assert(it.p->occupied());
+      controls[pos1].set_next(0);
+      assert(it.pc->occupied());
       return it;
     }
     else{
       for(std::size_t i=0;;++i){
         auto prev=minus_wrap(pos,i);
-        auto n=elements[prev].first();
+        auto n=controls[prev].first();
         if(n&&pos==plus_wrap(prev,n-1)){
-          elements[prev].set_first(0);
+          controls[prev].set_first(0);
           break;
         }
-        n=elements[prev].next();
+        n=controls[prev].next();
         if(n&&pos==plus_wrap(prev,n)){
-          elements[prev].set_next(0);
+          controls[prev].set_next(0);
           break;
         }
       }
-      assert(it.p->empty());
+      assert(it.pc->empty());
       return ++it;
     }
   }
@@ -249,7 +260,7 @@ private:
   foa_unordered_longhop_set(std::size_t n,Allocator al):
     al{al},size_index{size_policy::size_index(n)}
   {
-    elements.back().set_hash(0);
+    controls.back().set_hash(0);
   }
 
   template<typename Value>
@@ -329,10 +340,10 @@ private:
     std::size_t pos,
     foa_unordered_longhop_set& new_container,std::size_t& num_tx)
   {
-    auto n=elements[pos].first();
+    auto n=controls[pos].first();
     if(n){
       transfer_bucket_next(plus_wrap(pos,n-1),new_container,num_tx);
-      elements[pos].set_first(0);
+      controls[pos].set_first(0);
     }
   }
 
@@ -340,17 +351,17 @@ private:
     std::size_t pos,
     foa_unordered_longhop_set& new_container,std::size_t& num_tx)
   {
-    auto n=elements[pos].next();
+    auto n=controls[pos].next();
     if(n){
       transfer_bucket_next(plus_wrap(pos,n),new_container,num_tx);
-      elements[pos].set_next(0);
+      controls[pos].set_next(0);
     }
     if(new_container.unchecked_insert(
       std::move(elements[pos].value()))==new_container.end()){
       throw hopscotch_failure();
     }
     destroy_element(elements[pos].data());
-    elements[pos].reset();
+    controls[pos].reset();
     ++num_tx;
   }
 
@@ -377,32 +388,32 @@ private:
     while((n=minus_wrap(dst,prev))>=N){
       for(auto i=N-1;i;--i){
         auto mid=minus_wrap(dst,i);
-        auto j=elements[mid].first();
+        auto j=controls[mid].first();
         if(i<N-1 && j && j-1<i){
           auto hop=plus_wrap(mid,j-1);
-          auto k=elements[hop].next();
+          auto k=controls[hop].next();
           if(!k || j-1+k>i){  
             construct_element(
               std::move(elements[hop].value()),elements[dst].data());
-            elements[dst].set_hash(elements[hop].hash());
-            elements[dst].set_next(k? j-1+k-i : 0);
-            elements[hop].reset();
-            elements[mid].set_first(i+1);
+            controls[dst].set_hash(controls[hop].hash());
+            controls[dst].set_next(k? j-1+k-i : 0);
+            controls[hop].reset();
+            controls[mid].set_first(i+1);
             dst=hop;
             goto continue_;
           }
         }
-        j=elements[mid].next();
+        j=controls[mid].next();
         if(j && j<i){
           auto hop=plus_wrap(mid,j);
-          auto k=elements[hop].next();
+          auto k=controls[hop].next();
           if(!k || j+k>i){  
             construct_element(
               std::move(elements[hop].value()),elements[dst].data());
-            elements[dst].set_hash(elements[hop].hash());
-            elements[dst].set_next(k? j+k-i: 0);
-            elements[hop].reset();
-            elements[mid].set_next(i);
+            controls[dst].set_hash(controls[hop].hash());
+            controls[dst].set_next(k? j+k-i: 0);
+            controls[hop].reset();
+            controls[mid].set_next(i);
             dst=hop;
             goto continue_;
           }
@@ -418,16 +429,16 @@ private:
 #endif
     }
 
-    assert(elements[dst].empty());
+    assert(controls[dst].empty());
     construct_element(std::forward<Value>(x),elements[dst].data());  
-    elements[dst].set_hash(hash);
+    controls[dst].set_hash(hash);
     if(prev==minus_wrap(pos,1)){ // first element of bucket
-      elements[dst].set_next(elements[pos].first()?elements[pos].first()-n:0);
-      elements[pos].set_first(n);
+      controls[dst].set_next(controls[pos].first()?controls[pos].first()-n:0);
+      controls[pos].set_first(n);
     }
     else{
-      elements[dst].set_next(elements[prev].next()?elements[prev].next()-n:0);
-      elements[prev].set_next(n);
+      controls[dst].set_next(controls[prev].next()?controls[prev].next()-n:0);
+      controls[prev].set_next(n);
     }
     ++size_;
     return at(dst);
@@ -435,10 +446,10 @@ private:
 
   std::size_t find_empty_slot(std::size_t pos)const
   {
-    while(elements[pos].occupied())++pos;
+    while(controls[pos].occupied())++pos;
     if(pos<capacity_)return pos;
     pos=0;
-    while(elements[pos].occupied())++pos;
+    while(controls[pos].occupied())++pos;
     return pos;
   }
 
@@ -446,11 +457,11 @@ private:
   // returns pos-1 if result is first in bucket
   {
     auto d=minus_wrap(dst,pos);
-    auto n=elements[pos].first(),m=n-1;
+    auto n=controls[pos].first(),m=n-1;
     pos=minus_wrap(pos,1);
     while(n&&m<d){
       pos=plus_wrap(pos,n);
-      n=elements[pos].next();
+      n=controls[pos].next();
       m+=n;        
     }
     return pos;
@@ -459,11 +470,11 @@ private:
   std::size_t last_in_bucket(std::size_t pos)
   // returns pos-1 if bucket empty
   {
-    auto n=elements[pos].first();
+    auto n=controls[pos].first();
     pos=minus_wrap(pos,1);
     while(n){
       pos=plus_wrap(pos,n);
-      n=elements[pos].next();
+      n=controls[pos].next();
     }
     return pos;
   }
@@ -472,32 +483,32 @@ private:
   {
     auto pos0=pos,
          prev0=minus_wrap(pos,1);
-    pos=plus_wrap(pos,elements[pos].first()-1);
+    pos=plus_wrap(pos,controls[pos].first()-1);
     auto prev=move_to_end_of_bucket_and_erase(prev0,pos);
-    if(prev==prev0)elements[pos0].set_first(0);
-    else           elements[prev].set_next(0); 
+    if(prev==prev0)controls[pos0].set_first(0);
+    else           controls[prev].set_next(0); 
   }
 
   void erase_next(std::size_t prev)
   {
-    auto pos=plus_wrap(prev,elements[prev].next());
+    auto pos=plus_wrap(prev,controls[prev].next());
     prev=move_to_end_of_bucket_and_erase(prev,pos);
-    elements[prev].set_next(0); 
+    controls[prev].set_next(0); 
   }
 
   std::size_t move_to_end_of_bucket_and_erase(std::size_t prev,std::size_t pos)
   {
-    while(elements[pos].next()){
-      auto next=plus_wrap(pos,elements[pos].next());
+    while(controls[pos].next()){
+      auto next=plus_wrap(pos,controls[pos].next());
       std::swap(elements[pos].value(),elements[next].value());
-      auto hash_pos=elements[pos].hash();
-      elements[pos].set_hash(elements[next].hash());
-      elements[next].set_hash(hash_pos);
+      auto hash_pos=controls[pos].hash();
+      controls[pos].set_hash(controls[next].hash());
+      controls[next].set_hash(hash_pos);
       prev=pos;
       pos=next;
     }      
     destroy_element(elements[pos].data());
-    elements[pos].reset();
+    controls[pos].reset();
     --size_;
     return prev;
   }
@@ -505,14 +516,14 @@ private:
   template<typename Key>
   iterator find(const Key& x,std::size_t pos,std::size_t hash)const
   {
-    auto n=elements[pos].first();
+    auto n=controls[pos].first();
     if(n--)do{
       pos=plus_wrap(pos,n);
-      if(elements[pos].match(hash)&&
+      if(controls[pos].match(hash)&&
         BOOST_LIKELY(pred(x,elements[pos].value()))){
         return at(pos); 
       }
-      n=elements[pos].next();
+      n=controls[pos].next();
     }while(n);
     return end();
   }
@@ -521,16 +532,16 @@ private:
   std::size_t find_prev(const Key& x,std::size_t pos,std::size_t hash)const
   // returns pos-1 if result is first in bucket
   {
-    auto n=elements[pos].first();
+    auto n=controls[pos].first();
     pos=minus_wrap(pos,1);
     auto prev=pos;
     while(n){
       pos=plus_wrap(pos,n);
-      if(elements[pos].match(hash)&&
+      if(controls[pos].match(hash)&&
         BOOST_LIKELY(pred(x,elements[pos].value()))){
         return prev;
       }
-      n=elements[pos].next();
+      n=controls[pos].next();
       prev=pos;
     }
     return capacity_;
@@ -552,7 +563,8 @@ private:
   std::size_t   size_index=size_policy::size_index(size_);
   std::size_t   capacity_=size_policy::size(size_index);
   size_type     ml=max_load();
-  element_array elements{capacity_+1,al};
+  element_array elements{capacity_,al};
+  control_array controls{capacity_+1,al};
 
 #ifdef FXA_UNORDERED_LONGHOP_STATUS
   int           num_hops=0;
