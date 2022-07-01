c5e69946fcbe1d6542911f399dd775d12a65dcda
joaquintides <joaquin@tid.es>
2022-07-01 19:05:31 +0200

    micro-optimized non-linked find_impl

--- foa_unordered_nway.hpp
+++ foa_unordered_nway.hpp
@@ -1788,22 +1788,24 @@ private:
 #endif
       auto itg=groups.at(pos);
       auto mask=control(itg).match(short_hash);
-      if(mask)prefetch_elements(itg);
-      while(mask){
+      if(mask){
+        prefetch_elements(itg);
+        do{
 #ifdef FOA_UNORDERED_NWAYPLUS_STATUS
-        ++num_matches;
+          ++num_matches;
 #endif
         
-        FXA_ASSUME(mask!=0);
-        auto n=boost::core::countr_zero((unsigned int)mask);
-        if(BOOST_LIKELY(pred(x,elements(itg).at(n).value()))){
+          FXA_ASSUME(mask!=0);
+          auto n=boost::core::countr_zero((unsigned int)mask);
+          if(BOOST_LIKELY(pred(x,elements(itg).at(n).value()))){
 #ifdef FOA_UNORDERED_NWAYPLUS_STATUS
-          successful_find_runlengths+=runlength;
-          successful_find_runs+=1;  
+            successful_find_runlengths+=runlength;
+            successful_find_runs+=1;  
 #endif
-          return {itg,n};
-        }
-        mask&=mask-1;
+            return {itg,n};
+          }
+          mask&=mask-1;
+        }while(mask);
       }
         
       if(control(itg).check_empty()){
