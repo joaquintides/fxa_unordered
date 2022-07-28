f32ed499b0c8d534eff53da0b2e466add8bdfc98
Peter Dimov <pdimov@gmail.com>
2022-07-29 01:44:47 +0300

    Comment out xmx32, as it's dominated by xmx33

--- uint64.cpp
+++ uint64.cpp
@@ -342,8 +342,8 @@ int main()
     // test<foa_mxm32_unordered_rc15_map>( "foa_mxm32_unordered_rc15_map" );
     // test<foa_mxm33_unordered_rc16_map>( "foa_mxm33_unordered_rc16_map" );
     // test<foa_mxm33_unordered_rc15_map>( "foa_mxm33_unordered_rc15_map" );
-    test<foa_xmx32_unordered_rc16_map>( "foa_xmx32_unordered_rc16_map" );
-    test<foa_xmx32_unordered_rc15_map>( "foa_xmx32_unordered_rc15_map" );
+    // test<foa_xmx32_unordered_rc16_map>( "foa_xmx32_unordered_rc16_map" );
+    // test<foa_xmx32_unordered_rc15_map>( "foa_xmx32_unordered_rc15_map" );
     test<foa_xmx33_unordered_rc16_map>( "foa_xmx33_unordered_rc16_map" );
     test<foa_xmx33_unordered_rc15_map>( "foa_xmx33_unordered_rc15_map" );
     // test<foa_xmx34_unordered_rc16_map>( "foa_xmx34_unordered_rc16_map" );
