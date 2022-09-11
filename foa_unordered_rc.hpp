0869010bdb28f683312d355c3388c1bbcd679cc4
joaquintides <joaquin@tid.es>
2022-09-11 10:20:35 +0200

    dropped prefetching in find_impl (non-ARM)

--- foa_unordered_rc.hpp
+++ foa_unordered_rc.hpp
@@ -1065,8 +1065,7 @@ private:
 #if BOOST_ARCH_ARM
         prefetch_elements(pe);
 #else
-        prefetch(pe,std::false_type{});
-        prefetch(reinterpret_cast<const unsigned char*>(pe)+64,std::false_type{});
+        //prefetch(pe,std::false_type{});
 #endif
         do{
           auto n=unchecked_countr_zero((unsigned int)mask);
