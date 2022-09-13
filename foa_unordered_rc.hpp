3f41021ef26986325eacd5776d6d4b0390709378
joaquintides <joaquin@tid.es>
2022-09-13 10:00:43 +0200

    further disaggregated BOOST_LIKELY expression

--- foa_unordered_rc.hpp
+++ foa_unordered_rc.hpp
@@ -1077,8 +1077,10 @@ private:
           mask&=mask-1;
         }while(mask);
       }
-      if(BOOST_LIKELY(pg->is_not_overflowed(short_hash))||
-         BOOST_UNLIKELY(!pb.next(groups.size()))){
+      if(BOOST_LIKELY(pg->is_not_overflowed(short_hash))){
+        return end();
+      }
+      if(BOOST_UNLIKELY(!pb.next(groups.size()))){
         return end();
       }
     }
